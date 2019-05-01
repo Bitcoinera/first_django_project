d4ba3500401198f67e65e5f969fb4fe374e208af
anagutjor <anagjordano@gmail.com>
2019-05-01 19:32:01 +0200

    add assets. add partials

--- btre/urls.py
+++ btre/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    # path('about', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(req):
-    return HttpResponse('<h1>Hello World</h1>')
+    # return HttpResponse('<h1>Hello World</h1>')
+    return render(req, 'pages/index.html')
+
+def about(req):
+    return render(req, 'pages/about.html')
